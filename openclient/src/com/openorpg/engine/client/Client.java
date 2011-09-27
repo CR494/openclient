c80aa5f4461450dca333a7ef7a1f3752ad068acb
Raymond Cox <Raymond Cox@rjcomputer.transcendata.com>
2011-09-27 01:06:44 -0400

    initial commit

---
+++ openclient/src/com/openorpg/engine/client/Client.java
@@ -0,0 +1,14 @@
+package com.openorpg.engine.client;
+import java.util.logging.Logger;
+
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Client.class.getName());
+		logger.info("Hello client!");
+	}
+
+}
